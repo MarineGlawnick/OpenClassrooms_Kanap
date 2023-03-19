6c74925de4aa9b55a2c68521052193d12551d785
marine <marinebeatrice@hotmail.fr>
2023-03-19 15:47:03 +0100

    render products on index page

---
+++ front/js/script.js
@@ -0,0 +1,27 @@
+/* Rendering the products on the index page */
+function fetchProducts() {
+     /*calling the HTML section*/ 
+    const items = document.getElementById('items');  
+     /*fetching the products into the API*/
+    fetch('http://localhost:3000/api/products')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("success", data)
+            /*render each product*/
+            data.forEach(element => {
+                items.innerHTML += `<a href='./product.html?id=${element._id}'>
+           <article>
+             <img src='${element.imageUrl}' alt='${element.altTxt}'>
+             <h3 class='productName'>${element.name}</h3>
+             <p class='productDescription'>${element.description}</p>
+           </article>
+         </a>`
+            });
+        })
+        /*display error in case of problems with the API*/
+        .catch((error) => {
+            console.error("Sorry, the API is not working", error)
+        })
+}
+
+fetchProducts();
